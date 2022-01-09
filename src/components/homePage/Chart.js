d9bfd2fd3dd8561f9f7602a05527964c6240b839
ram-prasad-1 <ramprasaddeora@gmail.com>
2022-01-09 23:16:45 +0530

    add tooltip

--- src/components/homePage/Chart.js
+++ src/components/homePage/Chart.js
@@ -1,9 +1,9 @@
 import { useEffect, useState } from 'react';
 import Bubble from '@/components/homePage/Bubble';
 
-const rect_height = 160;
-const rect_width = 200;
-const rect_dy = 12;
+const rect_height = 36;
+const rect_width = 100;
+const rect_dy = 4;
 
 const Chart = ({ dataPoints }) => {
   const [activeItem, setActiveItem] = useState(null);
@@ -25,8 +25,8 @@ const Chart = ({ dataPoints }) => {
           <svg x={Math.floor(activeItem.x - rect_width/2)}
                 y={Math.floor(activeItem.y - activeItem.radius  - rect_dy - rect_height)}
                width={rect_width} height={rect_height + rect_dy}>
-            {/*<rect x="0" y="0" width={rect_width} height={rect_height} rx="6" ry="6" />*/}
-            <text x="20" y="35" className="text-sm text-red-600">My</text>
+            <rect x="0" y="0" width={rect_width} height={rect_height} rx="6" ry="6" />
+            <text x={rect_dy * 2} y={rect_dy*6} className="text-sm fill-white">{`${activeItem.salary}, ${activeItem.headcount}, ${activeItem.compratio}`}</text>
           </svg>
         )
       }
