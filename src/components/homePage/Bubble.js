d06275daea55b10ffbded30dca9244afeaf07c40
ram-prasad-1 <ramprasaddeora@gmail.com>
2022-01-09 23:05:53 +0530

    fix font sizing

--- src/components/homePage/Bubble.js
+++ src/components/homePage/Bubble.js
@@ -1,25 +1,37 @@
 import { useEffect, useState } from 'react';
 import { getSpanLength } from '@/utils/dom-utils';
 
-const Bubble = ({ item }) => {
-  const [fontSize, setFontSize] = useState('14px');
+const Bubble = ({ item, onMouseEnter,  onMouseLeave}) => {
+  const [textLength, setTextLength] = useState(null);
 
   useEffect(() => {
     if (getSpanLength(item.title) > 2 * item.radius) {
-      setFontSize('8px')
+      setTextLength(2 * item.radius - 10);
     }
   }, [item]);
 
+  const textProps = {
+    ...(textLength && {
+      fontSize: '10px',
+      textLength: textLength,
+      lengthAdjust:"spacingAndGlyphs"}),
+    ...(!textLength && {
+      fontSize: '14px'
+    })
+  }
+
   return (
     <>
-      <g>
+      <g onMouseEnter={() => onMouseEnter(item)}
+         onMouseLeave={() => onMouseLeave(item)}>
+
         <circle cx={item.x} cy={item.y} r={item.radius} fill={item.color} />
         <text
           x={item.x}
           y={item.y + 4}
           textAnchor="middle"
           fill="white"
-          fontSize={fontSize}
+          {...textProps}
         >
           {item.title}
         </text>
--- src/components/homePage/Chart.js
+++ src/components/homePage/Chart.js
@@ -1,12 +1,35 @@
 import { useEffect, useState } from 'react';
 import Bubble from '@/components/homePage/Bubble';
 
+const rect_height = 160;
+const rect_width = 200;
+const rect_dy = 12;
+
 const Chart = ({ dataPoints }) => {
+  const [activeItem, setActiveItem] = useState(null);
+
   return (
     <svg width={'1200'} height={'1200'} viewBox={`0 -600 1200 1200`}>
       {dataPoints.map((item, index) => {
-        return <Bubble key={index} item={item} />;
+        return (
+          <Bubble
+            key={index}
+            item={item}
+            onMouseEnter={(i) => setActiveItem(i)}
+            onMouseLeave={() => setActiveItem(null)}
+          />
+        );
       })}
+      {
+        activeItem && (
+          <svg x={Math.floor(activeItem.x - rect_width/2)}
+                y={Math.floor(activeItem.y - activeItem.radius  - rect_dy - rect_height)}
+               width={rect_width} height={rect_height + rect_dy}>
+            {/*<rect x="0" y="0" width={rect_width} height={rect_height} rx="6" ry="6" />*/}
+            <text x="20" y="35" className="text-sm text-red-600">My</text>
+          </svg>
+        )
+      }
     </svg>
   );
 };
